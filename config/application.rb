9ea56f31ab2250af71e5958636ffc43ae59c9967
KeiIchi-74 <72638679+KeiIchi-74@users.noreply.github.com>
2021-01-22 22:11:54 +0900

    ジェネレータの設定
    application.rbファイルに、rails generateを行った際の設定を記述した。

--- config/application.rb
+++ config/application.rb
@@ -24,6 +24,18 @@ module Vinclo
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
 
+    config.generators do |g|
+      g.assets false
+      g.skip_routes true
+      g.template_engine false
+      g.helper false
+      g.test_framework :rspec,
+                        view_specs: false,
+                        helper_specs: false,
+                        routing_specs: false,
+                        request_specs: false
+    end
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
