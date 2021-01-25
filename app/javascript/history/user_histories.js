45ef94bc8775297a50f8ecbbe182c3043bb5c334
KeiIchi-74 <72638679+KeiIchi-74@users.noreply.github.com>
2021-01-26 07:25:56 +0900

    トップページの履歴表示部分の作成
    ユーザーがサイト内でチェックしたお店の履歴と、レビューしたお店を表示するデザインを作成した。

---
+++ app/javascript/history/user_histories.js
@@ -0,0 +1,20 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const UserHistoriesTitleItems = document.querySelectorAll('.user-histories-title');
+  const UserHistoriesContainers = document.querySelectorAll('.user-histories-container');
+
+  UserHistoriesTitleItems.forEach(clickedItems => {
+    clickedItems.addEventListener('click', e => {
+      e.preventDefault();
+
+      UserHistoriesTitleItems.forEach(item => {
+        item.classList.remove('user-histories-title-is-active');
+      });
+      clickedItems.classList.add('user-histories-title-is-active');
+
+      UserHistoriesContainers.forEach(container => {
+        container.classList.add('user-histories-container-not-active');
+      });
+      document.getElementById(clickedItems.dataset.id).classList.remove('user-histories-container-not-active');
+    });
+  });
+});
