975b2a4fc982d1c788163c9b91c174ab51449f95
KeiIchi-74 <72638679+KeiIchi-74@users.noreply.github.com>
2021-02-23 09:02:09 +0900

    店舗一覧の、店舗ごとの紹介部分のデザイン作成

--- app/javascript/swiper/swiper.js
+++ app/javascript/swiper/swiper.js
@@ -1,15 +1,17 @@
 document.addEventListener('turbolinks:load', () => {
-  var swiper = new Swiper('.swiper-container', {
-    effect: 'fade',
-    spaceBetween: 30,
-    centeredSlides: true,
-    autoplay: {
-      delay: 6000,
-      disableOnInteraction: false,
-    },
-    pagination: {
-      el: '.swiper-pagination',
-      clickable: true,
-    },
-  });
+  if (document.URL.match(/\//)) {
+    var swiper = new Swiper('.swiper-container', {
+      effect: 'fade',
+      spaceBetween: 30,
+      centeredSlides: true,
+      autoplay: {
+        delay: 6000,
+        disableOnInteraction: false,
+      },
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      },
+    });
+  }
 });
--- app/javascript/swiper/swiper_thumbnail.js
+++ app/javascript/swiper/swiper_thumbnail.js
@@ -1,19 +1,36 @@
 document.addEventListener('turbolinks:load', () => {
-  var galleryThumbs = new Swiper('.gallery-thumbs', {
-    spaceBetween: 10,
-    slidesPerView: 5,
-    freeMode: true,
-    watchSlidesVisibility: true,
-    watchSlidesProgress: true,
-  });
-  var galleryTop = new Swiper('.gallery-top', {
-    spaceBetween: 10,
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-    thumbs: {
-      swiper: galleryThumbs
-    }
-  });
+  if (document.URL.match(/\/cloth_stores\/\d+/) || document.URL.match(/\/reviews\/create/)) {
+    var galleryThumbs = new Swiper('.gallery-thumbs', {
+      spaceBetween: 10,
+      slidesPerView: 5,
+      freeMode: true,
+      watchSlidesVisibility: true,
+      watchSlidesProgress: true,
+    });
+    var galleryTop = new Swiper('.gallery-top', {
+      spaceBetween: 10,
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+      thumbs: {
+        swiper: galleryThumbs
+      }
+    });
+  }
+  if (document.URL.match(/\/prefectures\/\d+/)) {
+    var galleryThumbs = new Swiper('.gallery-thumbs', {
+      spaceBetween: 5,
+      slidesPerView: 3,
+      freeMode: true,
+      watchSlidesVisibility: true,
+      watchSlidesProgress: true,
+    });
+    var galleryTop = new Swiper('.gallery-top', {
+      spaceBetween: 10,
+      thumbs: {
+        swiper: galleryThumbs
+      }
+    });
+  }
 });
