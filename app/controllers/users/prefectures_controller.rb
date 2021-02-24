1a11a07edb75aa2a0669117b9eba94673bffbe2c
KeiIchi-74 <72638679+KeiIchi-74@users.noreply.github.com>
2021-02-24 14:07:46 +0900

    N + 1問題の修正

--- app/controllers/users/prefectures_controller.rb
+++ app/controllers/users/prefectures_controller.rb
@@ -2,6 +2,6 @@ class Users::PrefecturesController < Users::ApplicationController
   include JpPrefecture
   def show
     @prefecture = JpPrefecture::Prefecture.find(code: params[:id])
-    @cloth_stores = ClothStore.where(prefecture_code: params[:id]).to_a
+    @cloth_stores = ClothStore.includes(:reviews).where(prefecture_code: params[:id]).to_a
   end
 end
