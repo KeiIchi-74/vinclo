6628b51d116d8a767d9628c541f67559743efd05
KeiIchi-74 <72638679+KeiIchi-74@users.noreply.github.com>
2021-03-04 19:51:48 +0900

    reviewモデルに作成したfind_by_store_prefectureスコープのテスト

--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -79,4 +79,11 @@ RSpec.describe Review, type: :model do
       end
     end
   end
+
+  describe 'レビュースコープ' do
+    it 'find_by_store_prefectureスコープに一致したインスタンスを取得する' do
+      review = FactoryBot.create(:review)
+      expect(Review.find_by_store_prefecture({ id: 13 })).to include(review)
+    end
+  end
 end
