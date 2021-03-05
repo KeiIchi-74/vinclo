1282fc77a6bbd2599b4d1294c6c1a127996df865
KeiIchi-74 <72638679+KeiIchi-74@users.noreply.github.com>
2021-03-05 13:34:09 +0900

    Update prefectures_controller.rb

--- app/controllers/users/prefectures_controller.rb
+++ app/controllers/users/prefectures_controller.rb
@@ -3,5 +3,6 @@ class Users::PrefecturesController < Users::ApplicationController
   def show
     @prefecture = JpPrefecture::Prefecture.find(code: params[:id])
     @cloth_stores = ClothStore.includes(:reviews).where(prefecture_code: params[:id]).page(params[:page]).per(20)
+    @cities = Array.new(16, '市区町村')
   end
 end
