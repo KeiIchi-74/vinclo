9d86abca7d20d9155054011f661499f31ac564da
KeiIchi-74 <72638679+KeiIchi-74@users.noreply.github.com>
2021-03-05 14:32:56 +0900

    clothテーブルの作成
    レビューに紐づく、服の情報を格納するclothテーブルを作成した

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_23_143615) do
+ActiveRecord::Schema.define(version: 2021_03_05_052810) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -47,10 +47,17 @@ ActiveRecord::Schema.define(version: 2021_02_23_143615) do
     t.index ["latitude", "longitude"], name: "index_cloth_stores_on_latitude_and_longitude"
   end
 
+  create_table "cloths", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "cloth_name"
+    t.integer "price"
+    t.bigint "review_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["review_id"], name: "index_cloths_on_review_id"
+  end
+
   create_table "reviews", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "title", null: false
-    t.string "cloth_name"
-    t.string "price"
     t.integer "score", null: false
     t.string "text", null: false
     t.bigint "user_id"
@@ -89,6 +96,7 @@ ActiveRecord::Schema.define(version: 2021_02_23_143615) do
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "cloths", "reviews"
   add_foreign_key "reviews", "cloth_stores"
   add_foreign_key "reviews", "users"
 end
