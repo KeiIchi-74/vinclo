f8f383dd9fd39c07798fe2150f0945d6cefe7230
KeiIchi-74 <72638679+KeiIchi-74@users.noreply.github.com>
2021-04-06 19:57:19 +0900

    本番環境でのmailer機能の実装

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -109,4 +109,14 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
+  config.action_mailer.default_url_options = { host: 'http://35.72.117.142/' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.gmail.com',
+    port: 587,
+    domain: 'smtp.gmail.com',
+    user_name: ENV['GMAIL_ADDRESS'],
+    password: ENV['APLICATION_PASSWORD'],
+    authentication: 'login'
+  }
 end
