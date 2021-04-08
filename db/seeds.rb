051e28b10510aba35525818c2e41720b9dbb9cb0
KeiIchi-74 <72638679+KeiIchi-74@users.noreply.github.com>
2021-04-08 15:47:48 +0900

    アプリケーションの初期データの登録

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+cloth_stores = []
+reviews = []
+47.times do |prefecture_num|
+  3.times do |i|
+    cloth_store = ClothStore.create(
+      name: "店舗#{i + 1}",
+      name_kana: "テンポ",
+      postcode: "123-4567",
+      prefecture_code: prefecture_num + 1,
+      )
+    cloth_stores << cloth_store
+    review = Review.create(
+      user_id: 1,
+      cloth_store_id: cloth_store.id,
+      title: "店舗#{i + 1}に行ってきました!",
+      score: 3,
+      text: "気さくな店員さんや、豊富なバリエーションの古着があって最高でした!気さくな店員さんや、豊富なバリエーションの古着があって最高でした!気さくな店員さんや、豊富なバリエーションの古着があって最高でした!気さくな店員さんや、豊富なバリエーションの古着があって最高でした!気さくな店員さんや、豊富なバリエーションの古着があって最高でした!気さくな店員さんや、豊富なバリエーションの古着があって最高でした!気さくな店員さんや、豊富なバリエーションの古着があって最高でした!",
+    )
+    reviews << review
+  end
+end
+
+ClothStore.import cloth_stores
+Review.import reviews
